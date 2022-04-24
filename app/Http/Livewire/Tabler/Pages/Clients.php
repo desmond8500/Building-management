0dca339d7763d360d0d5302ae1fca008f6303554
kratos <desmond8500@gmail.com>
2022-04-24 01:40:53 +0000

    fixes

--- app/Http/Livewire/Tabler/Pages/Clients.php
+++ app/Http/Livewire/Tabler/Pages/Clients.php
@@ -21,7 +21,9 @@ class Clients extends Component
     public function render()
     {
         return view('livewire.tabler.pages.clients', [
-            'clients' => Client::where('number', 'like', '%' . $this->search . '%')->paginate(10),
+            'clients' => Client::where('prenom', 'like', '%' . $this->search . '%')
+                    ->orWhere('nom', 'like', '%' . $this->search . '%')
+                    ->paginate(10),
         ])->extends('app.layout')->section('content');
     }
 
