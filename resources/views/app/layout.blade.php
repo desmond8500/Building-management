6451e08d03cc4cb5270584433f5caa01925ca51d
kratos <desmond8500@gmail.com>
2024-11-26 22:49:27 +0000

    faviscon add

--- resources/views/app/layout.blade.php
+++ resources/views/app/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Building Management</title>
+    <link rel="shortcut icon" href="{{ asset('img/icon/building.png') }}" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/css/tabler.min.css">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
