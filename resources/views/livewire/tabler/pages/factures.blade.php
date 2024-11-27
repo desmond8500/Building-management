05128aede4932524b393b696b29817a9299fb59d
kratos <desmond8500@gmail.com>
2024-11-27 02:49:51 +0000

    fix

--- resources/views/livewire/tabler/pages/factures.blade.php
+++ resources/views/livewire/tabler/pages/factures.blade.php
@@ -17,78 +17,81 @@
                         </div>
                     </div>
                 </div>
-                <table class="table table-responsive">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th class="text-nowrap">Appartement</th>
-                            <th class="text-nowrap">Montant</th>
-                            <th class="text-nowrap">Date</th>
-                            <th class="text-nowrap">Facture</th>
-                            <th width="20px" class="text-nowrap">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($factures as $key => $facture)
-                        <tr>
-                            @if ($facture_id == $facture->id)
-                                <div class="row p-2">
-                                    <div class="mb-3 col-md-6">
-                                        <label class="form-label">Compteurs</label>
-                                        <select wire:model.defer="compteur_id" class="form-control">
-                                            <option value="0">Sélectionnez un compteur</option>
-                                                @foreach ( $compteurs as $compteur)
-                                                    <option value="{{ $compteur->id }}">{{ $compteur->appartement->nom }} </option>
-                                                @endforeach
-                                        </select>
-                                    </div>
+                <div class="table-responsive">
+                    <table class="table ">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th class="text-nowrap">Appartement</th>
+                                <th class="text-nowrap">Montant</th>
+                                <th class="text-nowrap">Date</th>
+                                <th class="text-nowrap">Facture</th>
+                                <th width="20px" class="text-nowrap">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($factures as $key => $facture)
+                            <tr>
+                                @if ($facture_id == $facture->id)
+                                    <div class="row p-2">
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">Compteurs</label>
+                                            <select wire:model.defer="compteur_id" class="form-control">
+                                                <option value="0">Sélectionnez un compteur</option>
+                                                    @foreach ( $compteurs as $compteur)
+                                                        <option value="{{ $compteur->id }}">{{ $compteur->appartement->nom }} </option>
+                                                    @endforeach
+                                            </select>
+                                        </div>
 
-                                    <div class="mb-3 col-md-6">
-                                        <label class="form-label">Montant</label>
-                                        <input type="number" wire:model.defer="montant" class="form-control" placeholder="">
-                                    </div>
-                                    <div class="mb-3 col-md-6">
-                                        <label class="form-label">Date</label>
-                                        <input type="date" wire:model.defer="date" class="form-control" placeholder="">
-                                    </div>
-                                    <div class="mb-3 col-md-6">
-                                        <label class="form-label">Facture</label>
-                                        <input type="file" wire:model.defer="facture" class="form-control" placeholder="">
-                                    </div>
-                                    <div class="mb-3 col-md-6">
-                                        <label class="form-label">Numero de facture</label>
-                                        <input type="text" wire:model.defer="numero" class="form-control" placeholder="">
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="float-right">
-                                            <button wire:click="$set('facture_id', 0)" class="btn btn-secondary">Fermer</button>
-                                            <button wire:click="delete_facture" class="btn btn-danger">Supprimer</button>
-                                            <button wire:click="update_facture" class="btn btn-primary">Modifier</button>
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">Montant</label>
+                                            <input type="number" wire:model.defer="montant" class="form-control" placeholder="">
+                                        </div>
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">Date</label>
+                                            <input type="date" wire:model.defer="date" class="form-control" placeholder="">
+                                        </div>
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">Facture</label>
+                                            <input type="file" wire:model.defer="facture" class="form-control" placeholder="">
+                                        </div>
+                                        <div class="mb-3 col-md-6">
+                                            <label class="form-label">Numero de facture</label>
+                                            <input type="text" wire:model.defer="numero" class="form-control" placeholder="">
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="float-right">
+                                                <button wire:click="$set('facture_id', 0)" class="btn btn-secondary">Fermer</button>
+                                                <button wire:click="delete_facture" class="btn btn-danger">Supprimer</button>
+                                                <button wire:click="update_facture" class="btn btn-primary">Modifier</button>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
 
-                            @else
-                                <th>{{ $key+1 }}</th>
-                                <td>{{ $facture->compteur->appartement->nom }}</td>
-                                <td>{{ $facture->montant }}</td>
-                                <td>{{ $facture->date->format('d-m-Y') }}</td>
-                                <td>
-                                    <div>
-                                        <a href="{{ "storage/$facture->facture" }}" target="blank">{{ $facture->numero }}</a>
-                                    </div>
-                                </td>
-                                <td>
-                                    <button class="btn btn-outline-primary btn-icon" wire:click="edit_facture('{{ $facture->id }}')">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none" /> <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" /> <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" /> <line x1="16" y1="5" x2="19" y2="8" /> </svg>
-                                    </button>
-                                </td>
+                                @else
+                                    <th>{{ $key+1 }}</th>
+                                    <td>{{ $facture->compteur->appartement->nom }}</td>
+                                    <td>{{ $facture->montant }}</td>
+                                    <td>{{ $facture->date->format('d-m-Y') }}</td>
+                                    <td>
+                                        <div>
+                                            <a href="{{ "storage/$facture->facture" }}" target="blank">{{ $facture->numero }}</a>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-outline-primary btn-icon" wire:click="edit_facture('{{ $facture->id }}')">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none" /> <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" /> <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" /> <line x1="16" y1="5" x2="19" y2="8" /> </svg>
+                                        </button>
+                                    </td>
 
-                            @endif
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                                @endif
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                </div>
                 <div class="card-body">
                     {{ $factures->links() }}
                 </div>
